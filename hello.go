bbaae30108251f17efadf551c983774e8df0ac23
GrantOforiAmoafo <ebenamoafo222@gmail.com>
2026-04-20 12:17:00 +0000

    Refactor Hello function to support multiple languages and update tests accordingly

--- hello.go
+++ hello.go
@@ -1,11 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func Hello(name string) string {
-	return "Hello, " + name
+const (
+
+ french = "French"
+ spanish = "Spanish"
+ twi = "Twi"
+	
+ englishHelloPrefix ="Hello, "
+ spanishHelloPrefix = "Hola, "
+ frenchHelloPrefix = "Bonjour, "
+ twiHelloPrefix = "Akwaaba, "
+)
+
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french: 
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	case twi: 
+		prefix = twiHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world",""))
 }
--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,37 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("saying hello to people", func(t *testing.T){
+		got := Hello("Chris", "French")
+		want := "Bonjour, Chris"
 
+		assertCorrectMessage(t, got , want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T){
+		got := Hello("", "")
+		want:= "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("This is a failing test sample", func(t *testing.T){
+		got := Hello("Asamoah","Twi" )
+		want:= "Akwaaba, Asamoah"
+		assertCorrectMessage(t, got, want)
+
+	})
+}
+
+//This function is to reduce my failing error checks
+//for helper functions, it's a good idea too accept a `testing.TB` which is an interface
+//that *testing.T and *testing.B both satisfy
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper() //This is needed to tell the test suite that this method is a helper
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %q want %q", got , want )
 	}
 }
